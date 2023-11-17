f831aaf535c3fbbb351bb1bab1333de5f0a680bc
moccamocca <127862730+moccamocca@users.noreply.github.com>
2023-11-17 23:42:34 +0500

    Update settings.py

--- hotel/settings.py
+++ hotel/settings.py
@@ -95,16 +95,23 @@ WSGI_APPLICATION = 'hotel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "mssql",
+#        "NAME": "hotel",
+#        "USER": "Orlova-M-Y",
+#        "PASSWORD": "25367",
+#        "HOST": "ALEXX-PC\SQLEXPRESS",
+#        "OPTIONS": {"driver": "SQL Server Native Client 11.0",
+#                    },
+#    },
+#}
+
 DATABASES = {
-    "default": {
-        "ENGINE": "mssql",
-        "NAME": "hotel",
-        "USER": "Orlova-M-Y",
-        "PASSWORD": "25367",
-        "HOST": "ALEXX-PC\SQLEXPRESS",
-        "OPTIONS": {"driver": "SQL Server Native Client 11.0",
-                    },
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
